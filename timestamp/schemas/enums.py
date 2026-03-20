35a763255918e138002eeb759a199c6d36d512c9
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-03-21 07:55:42 +0800

    📦 new: added database engine

--- timestamp/schemas/enums.py
+++ timestamp/schemas/enums.py
@@ -5,3 +5,9 @@ class Role(str, Enum):
     EMPLOYEE = 1
     OFFICER = 2
     ADMIN = 3
+
+
+class Mode(str, Enum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TESTING = "testing"
