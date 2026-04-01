9d28ee7a1f963cc83461bb4dc8c32e013e646784
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-01 16:48:32 +0800

    ⚙️ setup: sync the serial index after inserting values

--- .init/1-inserts.sql
+++ .init/1-inserts.sql
@@ -11,3 +11,13 @@ INSERT INTO users (id, first_name, middle_name, last_name, email, password, role
 (1, 'John', 'Allister', 'Doe', 'john.doe@example.com', 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', 3),
 (2, 'Jane', 'Marie', 'Smith', 'jane.smith@example.com', 'c6ba91b90d922e159893f46c387e5dc1b3dc5c101a5a4522f03b987177a24a91', 2),
 (3, 'Alice', 'Grace', 'Johnson', 'alice.johnson@example.com', '5efc2b017da4f7736d192a74dde5891369e0685d4d38f2a455b6fcdab282df9c', 1);
+
+
+-- Reset sequences to match current data
+SELECT setval(pg_get_serial_sequence('users', 'id'),
+              (SELECT MAX(id) FROM users),
+              true);
+
+SELECT setval(pg_get_serial_sequence('role', 'id'),
+              (SELECT MAX(id) FROM role),
+              true);
